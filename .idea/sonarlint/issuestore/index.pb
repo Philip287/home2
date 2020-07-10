04e93f574475dcad2a0385deff50be84bdbbb098
Philip <f_suprun@mail.ru>
2020-07-10 19:58:10 +0300

    The firdt ex

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+L
+src/Homework3/Homework3.java,5\8\588a87c45de5a6ce863c6c81c6c4e46010ef6be4
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+I
+src/Fil/company/Main.java,6\e\6ed64fc7491eb421dc8b43c6fd422193f58af0a7
--- src/Homework3/Homework3.java
+++ src/Homework3/Homework3.java
@@ -1,8 +1,22 @@
 package Homework3;
 
+import java.util.Arrays;
+
 public class Homework3 {
     public static void main(String[] args) {
-
-
+        System.out.println("Рещение задачи №1, домашняя работа №3/ Сформировать строку, которая содержит " +
+                "100 случайных значений от 0 до 1000, разделённых пробелами (2 способа - String и StringBuilder)");
+        int STR_LENGTH = 100;
+        String str1 = "";
+        StringBuilder builder = new StringBuilder(str1);
+        for (int j = 0; j < STR_LENGTH; j++) {
+            int znachenie = (int) (Math.random() * 1000);
+            builder.append(znachenie + " ");
+        }
+        System.out.println(builder);
     }
+
 }
+
+
+
