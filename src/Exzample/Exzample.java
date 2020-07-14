f6b40ee5346e51969fec0ede8c610af73e60bf82
Philip <f_suprun@mail.ru>
2020-07-14 11:48:52 +0300

    0.1

--- src/Exzample/Exzample.java
+++ src/Exzample/Exzample.java
@@ -2,11 +2,29 @@ package Exzample;
 
 public class Exzample {
     public static void main(String[] args) {
-        printMessege();
+        printMessege("Philip");
+        printMessege("Suprun");
+        printMessege("Nikolaevich");
+
+        int a = 5;
+        int b = 10;
+        int rectanglSquare = calcRectangleSquare(a, b);
+        int square = calcSquare(b);
+        System.out.println("Rectangle square =" + square);
+        System.out.println("Rectangle square =" + rectanglSquare);
     }
 
-    static void printMessege() {
-        System.out.println("Hello Philip");
+    static void printMessege(String name) {
+        System.out.println("Hello " + name + "!!");
 
+    }
+
+    static int calcRectangleSquare(int x, int y) {
+        int square = x * y;
+        return square;
+    }
+
+    static int calcSquare(int x) {
+        return x * x;
     }
 }
